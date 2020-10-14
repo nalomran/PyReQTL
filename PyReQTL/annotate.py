23e75db60257a7532da95ec1db4ea5fb67d20db8
nalomran <nawafalomran@hotmail.com>
2020-10-14 11:33:25 +0300

    Rename main to cis_trans_annotator and run_command_lines to main and update docstring

--- PyReQTL/annotate.py
+++ PyReQTL/annotate.py
@@ -26,10 +26,12 @@ Output
 
 How to Run
 ----------
-    python annotate_cis_trans.py \
-    -r output/ReQTL_test_all_ReQTLs.txt \
-    -ga ../data/gene_locations_hg38.txt \
-    -o ReQTL_test
+    python -m PyReQTL.annotate \
+        -r output/ReQTL_test_all_ReQTLs.txt \
+        -ga ../data/gene_locations_hg38.txt \
+        -o ReQTL_test \
+        -c True
+
 
 * Python runtime via time command  8.19s user 0.61s system 112% cpu 7.838 total
 * R time command line 3.15s user 0.22s system 99% cpu 3.383 total
@@ -47,7 +49,12 @@ import rpy2.robjects.packages as rpackages  # type: ignore
 from rpy2.robjects import pandas2ri  # type: ignore
 from rpy2.robjects.packages import importr  # type: ignore
 
-from common import create_output_dir, output_filename_generator
+try:
+    from common import (create_output_dir, output_filename_generator,
+                        bool_conv_args)
+except ModuleNotFoundError:
+    from PyReQTL.common import (create_output_dir, output_filename_generator,
+                                bool_conv_args)
 
 # install the R package GenomicFeatures from within Python
 if not rpackages.isinstalled('GenomicFeatures'):
@@ -55,7 +62,6 @@ if not rpackages.isinstalled('GenomicFeatures'):
     bioc_manager = rpackages.importr('BiocManager')
     bioc_manager.install('GenomicFeatures')
     print("Done installing the package.")
-
 
 # importing the following required R packages to be used within Python
 print("Kindly wait for the required R packages to be imported into Python...")
@@ -75,34 +81,38 @@ print("Done importing.")
 pandas2ri.activate()
 
 
-def main(args) -> None:
-    """Annotates the output of ReQTL as cis or trans based on whether the
+def cis_trans_annotator(rqt_rst: str,
+                        gene_ann: str,
+                        out_prefx: str,
+                        cli: bool = False) -> None:
+    """Annotate the output of ReQTL as cis or trans based on whether the
     SNVs resides within its paired gene
 
     Parameter
     ---------
-    args: arguments from the command line
-        -r: the path to the ReQTL analysis result file
+    rqt_rst: the path to the ReQTL analysis result file
 
-        -ga: the path to the file gene location annotations
+    gene_ann: the path to the file gene location annotation
 
-        -o: the prefix for the output annotated result
+    out_prefx: the prefix for the output annotated result
+
+    cli: Whether the function is been executed with the command line.
+    Default is False.
 
     Return
-    -------
-    None
+    ------
+    reqtl_reslt_arranged: dataframe ReQTLs annotated as cis or trans
 
     Output
     ------
     - file with the ReQTLs annotated as cis or trans
-
 
     """
 
     start_time = datetime.now()
 
     # reading the ReQTL result file from run_matrix_ReQTL
-    reqtl_result = pd.read_table(args.rqt_rst, sep="\t")
+    reqtl_result = pd.read_table(rqt_rst, sep="\t")
 
     # ------------------------------------------------------------------------#
     # ------------------------------------------------------------------------#
@@ -127,19 +137,19 @@ def main(args) -> None:
         end=reqtl_reslt_arranged.start)
 
     # convert Python Pandas DataFrame to R-dataframe
-    reqtl_result_df_R = pandas2ri.py2rpy(reqtl_reslt_arranged)
+    reqtl_result_df_r = pandas2ri.py2rpy(reqtl_reslt_arranged)
 
     # read gene location file and then convert to R dataframe
-    gene_locs_py_df = pd.read_table(args.gene_ann, sep="\t")
+    gene_locs_py_df = pd.read_table(gene_ann, sep="\t")
     gene_locs_df_r = pandas2ri.py2rpy(gene_locs_py_df)
 
     # storing the location of genomic features for both R dataframes
-    reqtl_reslt_Granges_R = g_ranges.GRanges(reqtl_result_df_R)
-    gene_loc_Granges_R = g_ranges.GRanges(gene_locs_df_r)
+    reqtl_reslt_granges_r = g_ranges.GRanges(reqtl_result_df_r)
+    gene_loc_granges_r = g_ranges.GRanges(gene_locs_df_r)
 
     # finding the overlap between the ranges
-    overlaps = iranges.findOverlaps(reqtl_reslt_Granges_R,
-                                    gene_loc_Granges_R,
+    overlaps = iranges.findOverlaps(reqtl_reslt_granges_r,
+                                    gene_loc_granges_r,
                                     select="last",
                                     type="within")
 
@@ -173,18 +183,22 @@ def main(args) -> None:
     out_dir = create_output_dir("output")
 
     annotated_file = output_filename_generator(out_dir,
-                                               args.out_prefx,
+                                               out_prefx,
                                                "_ReQTLs_cistrans_ann.txt")
 
     reqtl_reslt_arranged.to_csv(annotated_file, sep="\t", index=False,
                                 na_rep='NULL')
 
     print(f"\nCis/trans annotated ReQTLs saved in {annotated_file}\n")
-    print(f"Analysis took after importing the required packages "
-          f"{(datetime.now() - start_time).total_seconds()} sec")
+
+    if cli:
+        print(f"Analysis took after importing the required packages "
+              f"{(datetime.now() - start_time).total_seconds()} sec")
+    else:
+        return reqtl_reslt_arranged
 
 
-def run_command_lines() -> None:
+def main() -> None:
     """Parses the command line arguments entered by the user
 
     Parameters
@@ -205,24 +219,33 @@ def run_command_lines() -> None:
                         dest="rqt_rst",
                         required=True,
                         help="the path to the ReQTL analysis result file")
-
     parser.add_argument('-ga',
                         dest='gene_ann',
                         required=True,
                         help="the path to the file gene location annotations")
-
     parser.add_argument('-o',
                         dest="out_prefx",
                         required=True,
                         help="the prefix for the output annotated result")
+    parser.add_argument("-c",
+                        dest="cli",
+                        default=False,
+                        type=bool_conv_args,
+                        help="""Whether the function is been executed with the 
+                        command line. Default is False!""")
 
     args = parser.parse_args()
 
+    rqt_rst = args.rqt_rst
+    gene_ann = args.gene_ann
+    out_prefx = args.out_prefx
+    cli = args.cli
+
     try:
-        main(args)
+        cis_trans_annotator(rqt_rst, gene_ann, out_prefx, cli)
     except KeyboardInterrupt:
         sys.exit('\nthe user ends the program')
 
 
 if __name__ == '__main__':
-    run_command_lines()
+    main()
