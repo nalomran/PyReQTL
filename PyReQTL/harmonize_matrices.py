10ea048fecde381461d8338cfb0904ee6ec2e031
nalomran <nawafalomran@hotmail.com>
2020-10-14 09:51:18 +0300

    rename function from main to harmonize_matrices and run_command_lines to main and apply some tweaks to this module

--- PyReQTL/harmonize_matrices.py
+++ PyReQTL/harmonize_matrices.py
@@ -15,10 +15,10 @@ Input + Options
 ----------------
 
     + -v: the path to the variant matrix generated from the module
-    build_VAF_matrix.py
+    vaf_matrix.py
 
     + -g: the path to the gene expression matrix generated from the module
-    build_gen_exp_matrix.py
+    gene_matrix.py
 
     + -c: the path to the covariate matrix located in the "data" folder
 
@@ -31,8 +31,11 @@ Return
 
 How to Run
 ----------
-      python harmonize_matrices.py -v output/ReQTL_test_VAF_matrix.txt \
-      -g output/ReQTL_test_gene-exp_matrix.txt -c ../data/covariates_matrix.txt
+      python -m PyReQTL.harmonize_matrices \
+      -v output/ReQTL_test_VAF_matrix.txt \
+      -g output/ReQTL_test_gene-exp_matrix.txt \
+      -cov data/covariates_matrix.txt \
+      -c True
 
 * Python runtime with time command 1.19s user 0.24s system 305% cpu 0.466 total
 * R time command line 1.33s user 0.13s system 96% cpu 1.504 total
@@ -46,27 +49,34 @@ from datetime import datetime
 
 import pandas as pd  # type: ignore
 
-from common import (create_output_dir, extract_filename_tag,
-                    output_filename_generator)
+try:
+    from common import (create_output_dir, output_filename_generator,
+                        bool_conv_args, extract_filename_tag)
+except ModuleNotFoundError:
+    from PyReQTL.common import (create_output_dir, output_filename_generator,
+                                bool_conv_args, extract_filename_tag)
 
 
-def main(args) -> None:
+def harmonize_matrices(var_dir: str,
+                       gex_dir: str,
+                       cov_dir: str,
+                       cli: bool = False):
     """Harmonizes matrices so the matrices can have the same samples.
 
     Parameter
     ---------
-    args: arguments from the command line
+    var_dir: the variant matrix generated from the script vaf_matrix.py
 
-        -v: the variant matrix generated from the script
-        build_VAF_matrix.py
+    gex_dir: the gene expression matrix generated from the script
+    gene_matrix.py
 
-        -g: the gene expression matrix generated from the script
-        build_gen_exp_matrix.py
+    cov_dir: the covariate matrix in the data folder
 
-        -c: the covariate matrix in the data folder
+    cli: Whether the function is been executed with the command line.
+    Default is False.
 
-    Returns
-    -------
+    Return
+    ------
     None
 
     Outputs
@@ -78,11 +88,11 @@ def main(args) -> None:
     start_time = datetime.now()
 
     # load in matrices into pandas dataframe for each input file
-    vaf_df = pd.read_table(args.var_dir, sep="\t")
+    vaf_df = pd.read_table(var_dir, sep="\t")
 
-    gene_express_df = pd.read_table(args.gex_dir, sep="\t")
+    gene_express_df = pd.read_table(gex_dir, sep="\t")
 
-    covar_df = pd.read_table(args.cov_dir, sep="\t")
+    covar_df = pd.read_table(cov_dir, sep="\t")
 
     # extract samples columns for each dataframe
     vaf_cols = vaf_df.iloc[:, 1:].columns
@@ -91,7 +101,7 @@ def main(args) -> None:
 
     covar_cols = covar_df.iloc[:, 1:].columns
 
-    # get the samples in all 3 matrices via pandas intersection
+    # get the samples in all 3 matrices with pandas intersection
     cols_intersect_tmp = vaf_cols.intersection(gene_express_cols)
     cols_intersect = list(cols_intersect_tmp.intersection(covar_cols))
 
@@ -107,22 +117,19 @@ def main(args) -> None:
 
     # writes result to files
     filename_var = output_filename_generator(output,
-                                             extract_filename_tag(
-                                                 args.var_dir),
+                                             extract_filename_tag(var_dir),
                                              '_harmonized.txt')
 
     vaf_df.to_csv(filename_var, sep="\t", index=False, na_rep='NA')
 
     filename_genexp = output_filename_generator(output,
-                                                extract_filename_tag(
-                                                    args.gex_dir),
+                                                extract_filename_tag(gex_dir),
                                                 '_harmonized.txt')
 
     gene_express_df.to_csv(filename_genexp, sep="\t", index=False, na_rep='NA')
 
     filename_cov = output_filename_generator(output,
-                                             extract_filename_tag(
-                                                 args.cov_dir),
+                                             extract_filename_tag(cov_dir),
                                              '_harmonized.txt')
 
     covar_df.to_csv(filename_cov, sep="\t", index=False, na_rep='NA')
@@ -133,11 +140,15 @@ def main(args) -> None:
 
     print(f"\nfile is for gene covariate matrix is saved to {filename_cov}\n")
 
-    print(f"Analysis took "
-          f"{(datetime.now() - start_time).total_seconds()} sec")
+    if cli:
+        print(f"Analysis took "
+              f"{(datetime.now() - start_time).total_seconds()} sec")
+
+    else:
+        return vaf_df, gene_express_df, covar_df
 
 
-def run_command_lines() -> None:
+def main() -> None:
     """Parses the command line arguments entered by the user
 
     Parameters
@@ -156,27 +167,35 @@ def run_command_lines() -> None:
                         dest='var_dir',
                         required=True,
                         help="""the path to the variant matrix generated from 
-                        the module 'build_VAF_matrix.py'""")
-
+                        the module 'vaf_matrix.py'""")
     parser.add_argument('-g',
                         dest='gex_dir',
                         required=True,
                         help="""the path to the gene expression matrix 
-                        generated from the module 'build_gen_exp_matrix.py'""")
-
-    parser.add_argument('-c',
+                        generated from the module 'gene_matrix.py'""")
+    parser.add_argument('-cov',
                         dest='cov_dir',
                         help="""the path to the covariate. In case this is not 
                         supplemented you have to remove the references to the 
                         covariate matrix in this very module""")
+    parser.add_argument("-c",
+                        dest="cli",
+                        default=False,
+                        type=bool_conv_args,
+                        help="""Whether the function is been executed with the 
+                        command line. Default is False!""")
 
     args = parser.parse_args()
+    var_dir = args.var_dir
+    gex_dir = args.gex_dir
+    cov_dir = args.cov_dir
+    cli = args.cli
 
     try:
-        main(args)
+        harmonize_matrices(var_dir, gex_dir, cov_dir, cli)
     except KeyboardInterrupt:
         sys.exit('\nthe user ends the program')
 
 
 if __name__ == '__main__':
-    run_command_lines()
+    main()
