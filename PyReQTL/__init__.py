6d97d9a4cbf6e2a7af0886ef836bcc70b2835be7
nalomran <nawafalomran@hotmail.com>
2020-10-17 09:39:48 +0300

    Add black line to __init__.py in docstring to follow pep257

--- PyReQTL/__init__.py
+++ PyReQTL/__init__.py
@@ -1,2 +1,6 @@
-"""Package initialization it invoked when the module in the package is
-imported. Will leave it blank for now and will add import statements later"""
+"""
+Package initialization it invoked when the module in the package is
+
+imported. Will leave it blank for now and will add import statements later.
+
+"""
